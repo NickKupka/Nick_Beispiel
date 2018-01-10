7347bdf130c15a539128a1c0c509bb30e9b7e671
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 15:43:48 +0100

    ein commit auf dem developer branch - die daten dürfen nur hier sein

--- src/massive_Package/geilerKlassenname.java
+++ src/massive_Package/geilerKlassenname.java
@@ -4,6 +4,8 @@ public class geilerKlassenname {
 
 	public static void main(String[] args) {
 		System.out.println("Was geht ab ihr sexy typen.");
+		System.out.println("Change im developer branch aber nicht in den anderen Branches.");
+		
 	}
 
 }
