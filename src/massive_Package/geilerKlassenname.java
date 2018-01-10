15af351279756e1ba63f4d317d7b20078dc9058a
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 15:50:17 +0100

    den commit will ich gleich wieder zurückbauen im developer branch

--- src/massive_Package/geilerKlassenname.java
+++ src/massive_Package/geilerKlassenname.java
@@ -5,7 +5,7 @@ public class geilerKlassenname {
 	public static void main(String[] args) {
 		System.out.println("Was geht ab ihr sexy typen.");
 		System.out.println("Change im developer branch aber nicht in den anderen Branches.");
-		
+		System.out.println("Commit mit Zeile 3 ich will das zurückbauen damit die verschwindet.");
 	}
 
 }
